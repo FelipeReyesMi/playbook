7141848411eed46afbf50c883207b6bf0ebd9daf
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-04-23 17:41:26 -0500

    Colocando segunda prueba llamando al metodo getInfo() de la clase

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,13 +1,20 @@
 const Spiderman = require('../app/spiderman')
 describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
+    test('Case 1 Dummy', () => { //Primer Test de Prueba
         //Instancia de la clase Spiderman
-      const andrewGarfield = new Spiderman("Spider-Man Sony",31,"Andrew Garfield",2,"Sony")
-      //Validaciones de las pruebas
-      expect(andrewGarfield.name).toBe("Spider-Man Sony");
-      expect(andrewGarfield.age).toBe(31);
-      expect(andrewGarfield.actor).toBe("Andrew Garfield");
-      expect(andrewGarfield.movies).toBe(2);
-      expect(andrewGarfield.studio).toBe("Sony");
+        const andrewGarfield = new Spiderman("Spider-Man Sony", 31, "Andrew Garfield", 2, "Sony")
+        //Validaciones de las pruebas
+        expect(andrewGarfield.name).toBe("Spider-Man Sony");
+        expect(andrewGarfield.age).toBe(31);
+        expect(andrewGarfield.actor).toBe("Andrew Garfield");
+        expect(andrewGarfield.movies).toBe(2);
+        expect(andrewGarfield.studio).toBe("Sony");
     });
-  })
+
+    test('Case 2 Use the method getInfo()', () => {//Segundo Test de prueba
+        //Instancia de la clase Spiderman con thomHoland
+        const thomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel studio") //Colocamos los elemetos que toma el contructor
+        //Validacion de la prueba
+        expect(thomHolland.getInfo()).toBe("Hey, I´m Tom Holland from Marvel studio");
+    });
+})
