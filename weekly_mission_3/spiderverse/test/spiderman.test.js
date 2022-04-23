80815b3a885feb7b5c4bd2783836be1216981eb9
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-04-23 16:15:25 -0500

    Prueba y error para el test

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,6 +1,13 @@
+const Spiderman = require('../app/spiderman')
 describe("Test Suite Dummy Description", () => {
     test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(3);
+        //Instancia de la clase Spiderman
+      const andrewGarfield = new Spiderman("Spider-Man Sony",31,"Andrew Garfield",2,"Sony")
+      //Validaciones de las pruebas
+      expect(andrewGarfield.name).toBe("Spider-Man Sony");
+      expect(andrewGarfield.age).toBe(31);
+      expect(andrewGarfield.actor).toBe("Andrew Garfield");
+      expect(andrewGarfield.movies).toBe(2);
+      expect(andrewGarfield.studio).toBe("Sony");
     });
   })
