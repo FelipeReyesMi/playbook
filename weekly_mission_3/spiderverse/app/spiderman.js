dbf70a4a99b0c7e86cf167cf9ff95fa533e592e7
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-04-23 17:40:18 -0500

    Agregando metodo a la clase

--- weekly_mission_3/spiderverse/app/spiderman.js
+++ weekly_mission_3/spiderverse/app/spiderman.js
@@ -1,11 +1,16 @@
-class Spiderman {
-    constructor(name,age,actor,movies,studio){
+class Spiderman {//Creacion de la clase Spiderman
+    constructor(name, age, actor, movies, studio) { //Creacion del metodo que tomara 5 parametros
+        //Declaración de los apartados que tomaran el valor que se le de 
         this.name = name,
-        this.age = age,
-        this.actor = actor,
-        this.movies = movies,
-        this.studio = studio
+            this.age = age,
+            this.actor = actor,
+            this.movies = movies,
+            this.studio = studio
+    }
+
+    getInfo() {//Declaracion del metodo que sera llamado 
+        return `Hey, I´m ${this.actor} from ${this.studio}` //Resultado que se nos devolvera al llamar el metodo
     }
 }
 
-module.exports = Spiderman
+module.exports = Spiderman //Exportacion de la clase
