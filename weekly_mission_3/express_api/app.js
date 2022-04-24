3bc1306f03b8b4dd6b2c314cca6ee8e76a465e57
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-04-24 18:18:47 -0500

    DELETE eliminar un explorer

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -46,3 +46,11 @@ app.put('/v1/explorers/:id', (req,res) => {
     const requestBody = req.body
     res.status(200).json({message: "Updated!"})
 })
+
+//DELETE eliminar un explorer
+app.delete('/v1/explorers/:id', (req,res) => {
+    console.log(`Api Explorers DELETE requests ${new Date()}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body
+    res.status(200).json({message: "Deleted!"})
+})
