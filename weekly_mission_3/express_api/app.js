54e073f748d94ee073d795283433e594f489ced1
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-04-24 17:47:10 -0500

    Mi primera API

---
+++ weekly_mission_3/express_api/app.js
@@ -0,0 +1,24 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+app.use(express.json()) // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
+
+//HTTP METHODS
+app.get('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers GET ALL requests ${new Date()}`)
+    const explorer1 = {id: 1,name:"Felipe 1"}
+    const explorer2 = {id: 2,name:"Felipe 2"}
+    const explorer3 = {id: 3,name:"Felipe 3"}
+    const explorer4 = {id: 4,name:"Felipe 4"}
+    const explorer5 = {id: 5,name:"Felipe 5"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4, explorer5]
+    res.status(200).json(explorers)
+})
