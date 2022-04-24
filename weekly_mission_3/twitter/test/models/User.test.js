8ad1f29ae5f8a5bd93b2cb074ce6e9d92682dfc1
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-04-23 20:16:16 -0500

    Realización del Requerimiento 2

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -4,14 +4,14 @@ describe("",()=>{
 
     test('Create an User object',()=>{
         //Creacion del obejto, instancia de la clase
-        const user = new User(1, "FelipeReyesMi","Felipe","Bio","dateCreated","lastUpdate")
+        const user = new User(1, "FelipeReyesMi","Felipe","Bio")
         //Aqui validamos los resultados de nuestro codigo
         //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha
         expect(user.id).toBe(1)
         expect(user.username).toBe("FelipeReyesMi")
         expect(user.name).toBe("Felipe")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdate).toBe("lastUpdate")
+        expect(user.dateCreated).not.toBeUndefined()//Verificamos que el valor no sea undefine
+        expect(user.lastUpdate).not.toBeUndefined()
     });
 })
