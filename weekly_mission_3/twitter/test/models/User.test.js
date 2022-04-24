c1f13df7aa3a6e01dadc274eec6c4a615079bd0f
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-04-23 21:34:34 -0500

    Realización del Requerimiento 3

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -14,4 +14,16 @@ describe("",()=>{
         expect(user.dateCreated).not.toBeUndefined()//Verificamos que el valor no sea undefine
         expect(user.lastUpdate).not.toBeUndefined()
     });
+    test('Add getters',()=>{
+        //Creacion del obejto, instancia de la clase
+        const user = new User(1, "FelipeReyesMi","Felipe","Bio")
+        //Aqui validamos los resultados de nuestro codigo
+        //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha
+        
+        expect(user.getUsername).toBe("FelipeReyesMi")
+       
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()//Verificamos que el valor no sea undefine
+        expect(user.getLastUpdate).not.toBeUndefined()
+    });
 })
