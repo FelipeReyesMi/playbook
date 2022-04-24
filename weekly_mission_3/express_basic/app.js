6dac717404e758e7f54c73ea0e06ef75cd1bc1f5
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-04-24 16:36:51 -0500

    Agregando parametros para la url

--- weekly_mission_3/express_basic/app.js
+++ weekly_mission_3/express_basic/app.js
@@ -15,3 +15,23 @@ app.get('/', (req,res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+//Respondiendo texto
+//localhost:3000/launchx
+app.get('/launchx',(req,res) => {
+    res.send('Bienvenidos a Launchx')
+})
+
+//Regresando un objeto
+//localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer={name:"Felipe", msg: "Hello"}
+    res.send(explorer)
+})
+
+// Query Params: Recibir parametros por la url
+// http://localhost:3000/explorers/felipe
+// req.params = {"explorerName":"carlo"}
+app.get('/explorers/:explorerName',(req,res) => {
+    res.send(req.params)
+})
