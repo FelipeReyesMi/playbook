63b5c921d36e71094f3eb40f5c740524a2f52c9d
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-04-23 21:53:38 -0500

    Realización del Requerimiento 4

--- weekly_mission_3/twitter/app/models/User.js
+++ weekly_mission_3/twitter/app/models/User.js
@@ -1,28 +1,37 @@
-class User{
-    constructor(id,username,name,bio){
+class User {
+    constructor(id, username, name, bio) {
         this.id = id,
-        this.username = username,
-        this.name = name,
-        this.bio = bio,
-        this.dateCreated = new Date(),
-        this.lastUpdate = new Date()//Metodo Date() nos da la fecha del dia en el que se ejecuta
+            this.username = username,
+            this.name = name,
+            this.bio = bio,
+            this.dateCreated = new Date(),
+            this.lastUpdate = new Date() //Metodo Date() nos da la fecha del dia en el que se ejecuta
     }
 
-    get getUsername(){
+    //Creacion de los getters 
+    get getUsername() {
         return this.username
     }
 
-    get getBio(){
+    get getBio() {
         return this.bio
     }
 
-    get getDateCreated(){
+    get getDateCreated() {
         return this.dateCreated
     }
 
-    get getLastUpdate(){
+    get getLastUpdate() {
         return this.lastUpdate
     }
+    //Creacion de los setters
+    set setUsername(newUsername) {
+        this.username = newUsername
+    }
+    set setBio(newBio) {
+        this.bio = newBio
+    }
+
 }
 
 module.exports = User
--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -1,29 +1,40 @@
 const User = require('./../../app/models/User')
 
-describe("",()=>{
+describe("", () => {
 
-    test('Create an User object',()=>{
+    test('Create an User object', () => {
         //Creacion del obejto, instancia de la clase
-        const user = new User(1, "FelipeReyesMi","Felipe","Bio")
+        const user = new User(1, "FelipeReyesMi", "Felipe", "Bio")
         //Aqui validamos los resultados de nuestro codigo
         //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha
         expect(user.id).toBe(1)
         expect(user.username).toBe("FelipeReyesMi")
         expect(user.name).toBe("Felipe")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).not.toBeUndefined()//Verificamos que el valor no sea undefine
+        expect(user.dateCreated).not.toBeUndefined() //Verificamos que el valor no sea undefine
         expect(user.lastUpdate).not.toBeUndefined()
     });
-    test('Add getters',()=>{
+
+    test('Add getters', () => {
         //Creacion del obejto, instancia de la clase
-        const user = new User(1, "FelipeReyesMi","Felipe","Bio")
+        const user = new User(1, "FelipeReyesMi", "Felipe", "Bio")
         //Aqui validamos los resultados de nuestro codigo
         //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha
-        
         expect(user.getUsername).toBe("FelipeReyesMi")
-       
         expect(user.getBio).toBe("Bio")
-        expect(user.getDateCreated).not.toBeUndefined()//Verificamos que el valor no sea undefine
+        expect(user.getDateCreated).not.toBeUndefined() //Verificamos que el valor no sea undefine
         expect(user.getLastUpdate).not.toBeUndefined()
+    });
+
+    test('Add setters', () => {
+        //Creacion del obejto, instancia de la clase
+        const user = new User(1, "FelipeReyesMi", "Felipe", "Bio")
+        //Aqui validamos los resultados de nuestro codigo
+        //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha
+        user.setUsername = "Gilmar"
+        expect(user.username).toBe("Gilmar")
+
+        user.setBio = "New bio"
+        expect(user.bio).toBe("New bio")
     });
 })
