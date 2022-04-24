81e021454e6306965d9d4b6c66ee8eaf92b2ab55
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-04-24 18:05:06 -0500

    Explorer mediante id

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -22,3 +22,11 @@ app.get('/v1/explorers', (req,res) => {
     const explorers = [explorer1, explorer2, explorer3, explorer4, explorer5]
     res.status(200).json(explorers)
 })
+
+app.get('/v1/explorers/:id', (req,res) => {
+    console.log(`Api Explorers GET requests ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id: 1,name:"Felipe"}
+    
+    res.status(200).json(explorer)
+})
