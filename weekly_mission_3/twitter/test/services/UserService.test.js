d62c1af300c407302a162e47f1efc0e31a8bae00
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-04-24 13:01:35 -0500

    Requerimiento 2

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -8,6 +8,15 @@ describe("Tests for UserService", () => {
         expect(user.name).toBe("Felipe")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
+    });
+
+    test("2. Get all user data in a list", () => {
+        const user = UserService.create(1, "FelipeReyesMi", "Felipe")
+        const userInfoInList = UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("FelipeReyesMi")
+        expect(userInfoInList[2]).toBe("Felipe")
+        expect(userInfoInList[3]).toBe("Sin bio")
     })
 
 })
