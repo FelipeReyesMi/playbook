23363553c7b0394c48ef6114da63968bbef77fbb
FelipeReyesMi <freyesm006@alumno.uaemex.mx>
2022-05-02 19:39:49 -0500

    Agragando pruebas

---
+++ weekly_mission_4/Refactoring/test/Fizzbuzz.test.js
@@ -0,0 +1,29 @@
+const ExplorerService = require('../lib/services/ExplorerService');
+const FizzbuzzService = require('../lib/services/FizzbuzzService');
+const Reader = require('./../lib/utils/Reader')
+const explorers =  Reader.readJsonFile("explorers.json")
+
+describe("Refactor de la lectura del archivo", () =>{
+
+    test('Caso de prueba 1: Cuantos explorers existen',() =>{
+        
+        const nombres = explorers.filter((explorer) => explorer.name);
+        const nombre = nombres.map((explorer) => explorer.name);
+        const long = nombre.length
+        expect(long).toBe(15)
+    });
+
+    test('Caso de prueba 2: Obtener el numero de los nombres de los explorers en el curso de java',() =>{
+        const curso = ExplorerService.getExplorersUsernamesByMission(explorers, "java")
+        const long = curso.length
+        expect(long).toBe(5)
+    });
+
+    test('Caso de prueba 3: FIZZBUZZ',() =>{
+        const alumno = {name: "Francisco", score: 30}
+        const curso = FizzbuzzService.applyValidationInExplorer(alumno)
+        const res = curso.trick
+        expect(res).toBe("FIZZBUZZ")
+    });
+})
+
